e5c909e6d9d6de42c2d74421e1fe7fbd3d104589
sheiguey <40198193+sheiguey@users.noreply.github.com>
2024-02-21 14:07:08 +0100

    update map view final

--- src/app/pages/real-time/components/map/map.component.ts
+++ src/app/pages/real-time/components/map/map.component.ts
@@ -18,7 +18,7 @@ export class MapComponent implements OnInit {
   private initMap():void{
     var map = L.map('map').setView([51.505, -0.09], 13);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     }).addTo(map);
 
     L.marker([51.5, -0.09]).addTo(map)
