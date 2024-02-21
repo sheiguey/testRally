78d3fc0f15e6d515294e5bab7d4b8c8575a0f251
sheiguey <40198193+sheiguey@users.noreply.github.com>
2024-02-21 14:43:13 +0100

    update map view final

--- src/app/pages/real-time/components/map/map.component.ts
+++ src/app/pages/real-time/components/map/map.component.ts
@@ -19,8 +19,9 @@ export class MapComponent implements OnInit {
     var map = L.map('map').setView([51.505, -0.09], 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        
-    }).addTo(map);
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+     }).addTo(map)
 
     L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
