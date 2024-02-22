5a2791ab2d93d0c99c2f61ed734cc6fc1359a60d
sheiguey <40198193+sheiguey@users.noreply.github.com>
2024-02-22 06:50:50 +0100

    new implementation

--- src/app/pages/real-time/components/map/map.component.ts
+++ src/app/pages/real-time/components/map/map.component.ts
@@ -12,10 +12,68 @@ import { GoogleMap } from '@angular/google-maps';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class MapComponent implements OnInit {
+export class MapComponent {
   private map!: L.Map;
+  markers: L.Marker[] = [];
 
-  private initMap():void{
+  options = {
+    layers: [
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      })
+    ],
+    zoom: 16,
+    center: { lat: 28.626137, lng: 79.821603 }
+  }
+
+  initMarkers() {
+    const initialMarkers = [
+      {
+        position: { lat: 28.625485, lng: 79.821091 },
+        draggable: true
+      },
+      {
+        position: { lat: 28.625293, lng: 79.817926 },
+        draggable: false
+      },
+      {
+        position: { lat: 28.625182, lng: 79.81464 },
+        draggable: true
+      }
+    ];
+    for (let index = 0; index < initialMarkers.length; index++) {
+      const data = initialMarkers[index];
+      const marker = this.generateMarker(data, index);
+      marker.addTo(this.map).bindPopup(`<b>${data.position.lat},  ${data.position.lng}</b>`);
+      this.map.panTo(data.position);
+      this.markers.push(marker)
+    }
+  }
+
+  generateMarker(data: any, index: number) {
+    return L.marker(data.position, { draggable: data.draggable })
+      .on('click', (event) => this.markerClicked(event, index))
+      .on('dragend', (event) => this.markerDragEnd(event, index));
+  }
+
+  onMapReady($event: L.Map) {
+    this.map = $event;
+    this.initMarkers();
+  }
+
+  mapClicked($event: any) {
+    console.log($event.latlng.lat, $event.latlng.lng);
+  }
+
+  markerClicked($event: any, index: number) {
+    console.log($event.latlng.lat, $event.latlng.lng);
+  }
+
+  markerDragEnd($event: any, index: number) {
+    console.log($event.target.getLatLng());
+  }
+
+ /*  private initMap():void{
     this.map = L.map('map').setView([51.505, -0.09], 13);
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -30,12 +88,12 @@ export class MapComponent implements OnInit {
     .bindPopup('conducteur 1')
     .openPopup();
    }
-
+ */
   constructor(){}
 
-  ngOnInit(): void {
-    this.initMap()
-  }
+/*   ngOnInit(): void {
+ this.initMap()
+  } */
    
 
 }
