dbf462c0c0a411df7fc77cc05c51d86ce69c0ff7
sheiguey <40198193+sheiguey@users.noreply.github.com>
2024-02-22 13:31:57 +0100

    realtime service

--- src/app/pages/real-time/components/map/map.component.ts
+++ src/app/pages/real-time/components/map/map.component.ts
@@ -27,7 +27,6 @@ export class MapComponent implements OnInit {
   ngOnInit(): void {
     this.initMap()
     this.onUpdatePosition()
-    
    } 
       
   constructor(private realTimeService:RealTimeService){}
@@ -49,59 +48,22 @@ export class MapComponent implements OnInit {
   onGetVehiclePosition(param:string){
     this.realTimeService.getVehiclePosition(param)
     .then(res=>{
-      this.vehiclePosition=res.data
+      this.vehiclePosition=res.data.items
     })
     .catch(err => console.log(err))
     .finally(()=>console.log(this.vehiclePosition))
   }
 
   initMarkers() {
-    const initialMarkers = [
-      {
-        position: { lat: 28.625485, lng: 79.821091 },
-        draggable: true
-      },
-      {
-        position: { lat: 28.625293, lng: 79.817926 },
-        draggable: false
-      },
-      {
-        position: { lat: 28.625182, lng: 79.81464 },
-        draggable: true
-      }
-    ];
-    for (let index = 0; index < initialMarkers.length; index++) {
-      const data = initialMarkers[index];
-      const marker = this.generateMarker(data, index);
-      marker.addTo(this.map).bindPopup(`<b>${data.position.lat},  ${data.position.lng}</b>`);
-      this.map.panTo(data.position);
-      this.markers.push(marker)
-    }
+    L.marker([51.5, -0.09]).addTo(this.map)
   }
 
-  generateMarker(data: any, index: number) {
-    return L.marker(data.position, { draggable: data.draggable })
-      .on('click', (event) => this.markerClicked(event, index))
-      .on('dragend', (event) => this.markerDragEnd(event, index));
-  }
+  
 
   onMapReady($event: L.Map) {
     this.map = $event;
     this.initMarkers();
   }
-
-  mapClicked($event: any) {
-    console.log($event.latlng.lat, $event.latlng.lng);
-  }
-
-  markerClicked($event: any, index: number) {
-    console.log($event.latlng.lat, $event.latlng.lng);
-  }
-
-  markerDragEnd($event: any, index: number) {
-    console.log($event.target.getLatLng());
-  }
-
 
  private initMap():void{
   this.options = {
@@ -112,6 +74,7 @@ export class MapComponent implements OnInit {
     ],
     zoom: 16,
     center: { lat: 28.626137, lng: 79.821603 }
+    
   }
 }
  
