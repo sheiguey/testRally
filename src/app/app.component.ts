cebc42b05293676f5a2725510c5038b254152cbe
sheiguey <40198193+sheiguey@users.noreply.github.com>
2024-02-22 07:00:52 +0100

    new implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';}
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 @Component({
   selector: 'app-root',
